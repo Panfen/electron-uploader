9b2ebe8bb0a1fb15cc8f6ac8696d84cfea7d8897
panfen <2809956575@qq.com>
2018-01-17 21:55:19 +0800

    'update'

--- src/renderer/components/LandingPage/Setting.vue
+++ src/renderer/components/LandingPage/Setting.vue
@@ -9,6 +9,21 @@
 			    	<el-input v-model.accesskey="qiniuForm.accesskey" size="medium"></el-input>
 			    </el-col>
 			  </el-form-item>
+			  <el-form-item label="SecretKey" prop="secretkey">
+			  	<el-col :span="12">
+			    	<el-input v-model.secretkey="qiniuForm.secretkey" size="medium"></el-input>
+			    </el-col>
+			  </el-form-item>
+			  <el-form-item label="存储空间" prop="storespace">
+			  	<el-col :span="12">
+			    	<el-input v-model.storespace="qiniuForm.storespace" size="medium"></el-input>
+			    </el-col>
+			  </el-form-item>
+			  <el-form-item label="上传网址" prop="uploadurl">
+			  	<el-col :span="12">
+			    	<el-input v-model.uploadurl="qiniuForm.uploadurl" size="medium"></el-input>
+			    </el-col>
+			  </el-form-item>
 			  <el-form-item>	
 			    <el-button type="primary" size="medium" @click="submitQiniuForm('qiniuForm')">确定</el-button>
 			    <el-button size="medium" @click="resetQiniuForm('qiniuForm')">重置</el-button>
@@ -34,15 +49,48 @@
     			callback();
     		}
     	}
+    	var checkQiniuSecretKey = (rule, value, callback) => {
+    		if(!value){
+    			return callback(new Error('SecretKey不能为空'));
+    		}else{
+    			callback();
+    		}
+    	}
+    	var checkQiniuStoreSpace = (rule, value, callback) => {
+    		if(!value){
+    			return callback(new Error('存储空间不能为空'));
+    		}else{
+    			callback();
+    		}
+    	}
+    	var checkQiniuUploadUrl = (rule, value, callback) => {
+    		if(!value){
+    			return callback(new Error('上传网址不能为空'));
+    		}else{
+    			callback();
+    		}
+    	}
       return {
         activeName: 'qiniu',
         qiniuForm: {
-        	accesskey: ''
+        	accesskey: '',
+        	secretkey: '',
+        	storespace: '',
+        	uploadurl: ''
         },
         qiniuRules: {
         	accesskey: [
         		{ validator: checkQiniuAccessKey, trigger: 'blur' }
-        	]
+        	],
+        	secretkey: [
+        		{ validator: checkQiniuSecretKey, trigger: 'blur' }
+        	],
+        	storespace: [
+        		{ validator: checkQiniuStoreSpace, trigger: 'blur' }
+        	],
+        	uploadurl:  [
+        		{ validator: checkQiniuUploadUrl, trigger: 'blur' }
+        	],
         }
       }
     },
